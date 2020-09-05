d38545ab51666b48ae4650c7285cb9055e7d0467
Matt Vaccaro <mattvaccaro@Matts-MBP.hsd1.tn.comcast.net>
2020-09-04 19:14:39 -0500

    complete

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,39 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(a, b):
+    # what will be returned once everything is sorted
+    finalMergeList = []
+    # We need something that will hold each side of the array to increament
+    indexA = 0
+    indexB = 0
+    # We need to loop over both arrays - Base case is if one index is no longer less then the counters
+    while indexA < len(a) and indexB < len(b):
+        # If a is less append it && inc
+        if a[indexA] < b[indexB]:
+            finalMergeList.append(a[indexA])
+            indexA += 1
+        # Append B and Inc
+        else:
+            finalMergeList.append(b[indexB])
+            indexB += 1
+    # Its very possible the lists wont append equally -
+    # If one list ends early we can extend the rest to the list
+    # If the len of a is = to its counter we know its done so extend B
+    if len(a) == indexA:
+        finalMergeList.extend(b[indexB:])
+    # else Extend A
+    else:
+        finalMergeList.extend(a[indexA:])
 
-    # Your code here
+    return finalMergeList
 
 
-    return merged_arr
+def merge_sort(a):
+    # If None
+    if len(a) <= 1:
+        return a
 
-# TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
-    # Your code here
+    # Continue to split the array down until it's singles
+    left, right = merge_sort(a[:len(a)//2]), merge_sort(a[len(a)//2:])
 
-
-    return arr
-
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
-# utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
-
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+    # Once there singles they will run through this code, and continue to get more and more merge
+    # Over the course of recurssion
+    return merge(left, right)
